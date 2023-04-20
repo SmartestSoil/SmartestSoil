592147d43bfa488095465e6a929b996994145b79
mufidaA <mufidaalakulju@gmail.com>
2023-04-20 13:20:44 +0300

    UI linked to sensors in both backends

--- app/src/main/java/com/example/smartestsoil/ui/screens/SensorChart.kt
+++ app/src/main/java/com/example/smartestsoil/ui/screens/SensorChart.kt
@@ -22,34 +22,51 @@ import com.patrykandpatrick.vico.core.chart.decoration.Decoration
 import com.patrykandpatrick.vico.core.chart.edges.FadingEdges
 import com.patrykandpatrick.vico.core.entry.FloatEntry
 import com.patrykandpatrick.vico.core.entry.entryModelOf
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
+import java.time.*
 import java.time.format.DateTimeFormatter
 
 
 @Composable
 fun SensorChart(sensorData: List<SensorData>, displaySensor: String) {
-    Log.d("in chart ","$ $displaySensor")
-    if (displaySensor.toString() == "sensor1") {
-    // move the chart-related code from the SensorList composable to this function
-    val currentDate = LocalDate.now(ZoneId.systemDefault())
+    Log.d("in view model and in sensordata ", "$displaySensor")
     val dateFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+
+    val latestDateUtc = sensorData
+        .map { OffsetDateTime.parse(it.timestamp, dateFormatter).toInstant().atZone(ZoneId.of("UTC")).toLocalDate() }
+        .maxOrNull()
+
     val todaysData = sensorData.filter { sensorData ->
-        val timestampDate = LocalDateTime.parse(sensorData.timestamp, dateFormatter).toLocalDate()
-        timestampDate == currentDate
+        Log.d("Filtering", "sensorData.sensor_id: ${sensorData.sensor_id}, displaySensor: $displaySensor")
+
+        val timestampDateTime = OffsetDateTime.parse(sensorData.timestamp, dateFormatter)
+        val timestampDateUtc = timestampDateTime.toInstant().atZone(ZoneId.of("UTC")).toLocalDate()
+
+        val isMatchingSensor = when (displaySensor) {
+            "sensor1" -> sensorData.sensor_id == "soil_sensor_001"
+            "sensor2" -> sensorData.sensor_id == "soil_sensor_002"
+            else -> false // handle other cases if needed
+        }
+
+        Log.d("Filtering", "isMatchingSensor: $isMatchingSensor, timestampDateUtc: $timestampDateUtc, latestDateUtc: $latestDateUtc")
+
+        isMatchingSensor && (timestampDateUtc == latestDateUtc)
+
     }
+    Log.d("comparaison time", "is ${LocalDate.now(ZoneOffset.UTC)}")
+
     val temperatureEntries = todaysData.mapIndexed { index, sensorData ->
         FloatEntry(index.toFloat(), sensorData.soil_temperature)
     }
     val moistureEntries = todaysData.mapIndexed { index, sensorData ->
         FloatEntry(index.toFloat(), sensorData.soil_moisture)
     }
+    Log.d("let see ","$temperatureEntries")
     val chartEntryModel = entryModelOf(temperatureEntries, moistureEntries)
     val itemsList = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
     //val chartEntryModel = entryModelOf(entriesOf(4f, 12f, 8f, 16f),
     //   entriesOf(12f, 16f, 4f, 12f))
     // Update the stored value when a new value is fetched
+
     Box(
         modifier = Modifier
             .fillMaxWidth(),
@@ -68,6 +85,5 @@ fun SensorChart(sensorData: List<SensorData>, displaySensor: String) {
                 .padding(16.dp) // Add some padding to the chart area
 
         )
-    }
     }
 }
